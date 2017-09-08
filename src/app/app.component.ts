855d4e98b9ee891a8016b5151e196025e6387089
THBar <choreno@gmail.com>
2017-09-08 15:59:07 -0400

    updating view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,4 +54,28 @@ export class AppComponent {
      */
   }
 
+
+
+
+ // Doughnut
+ public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+ public doughnutChartData:number[] = [350, 450, 100];
+ public doughnutChartType:string = 'doughnut';
+
+//  // events
+//  public chartClicked(e:any):void {
+//    console.log(e);
+//  }
+
+//  public chartHovered(e:any):void {
+//    console.log(e);
+//  }
+
+
+
+
+
+
+
+
 }
